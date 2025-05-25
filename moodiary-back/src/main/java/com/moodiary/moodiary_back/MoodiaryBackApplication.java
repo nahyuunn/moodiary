d7e594c05c58922c3eac5a5d3d9ab545b023f760
nahyuunn <138644012+nahyuunn@users.noreply.github.com>
2025-05-25 23:00:12 +0900

    chore: setup

---
+++ moodiary-back/src/main/java/com/moodiary/moodiary_back/MoodiaryBackApplication.java
@@ -0,0 +1,13 @@
+package com.moodiary.moodiary_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodiaryBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodiaryBackApplication.class, args);
+	}
+
+}
---
+++ moodiary-back/src/test/java/com/moodiary/moodiary_back/MoodiaryBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moodiary.moodiary_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodiaryBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
